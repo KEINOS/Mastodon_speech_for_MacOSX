37d31fc077c3c7a7e9b24926161e40805c40c923
KEINOS <github@keinos.com>
2018-02-03 13:43:50 +0900

    test: Echo directory paths to check

--- src/index.php
+++ src/index.php
@@ -2,5 +2,21 @@
 require_once('constants.php.inc');
 require_once('functions.php.inc');
 
-echo 'Hello World' . PHP_EOL;
+$path_json_qiitedon = './qiitedon.json';
 
+echo '__FILE__       :' . __FILE__        . PHP_EOL;
+echo 'Dir __FILE__   :' . dirname(__FILE__) . PHP_EOL;
+echo 'Phar path true :' . Phar::running() . PHP_EOL;
+echo 'Phar path false:' . Phar::running(false) . PHP_EOL;
+echo 'Phar dir       :' . dirname(Phar::running(false)) . PHP_EOL;
+echo 'Real path:' . realpath('./')  . PHP_EOL;
+
+if (!file_exists($path_json_qiitedon)) {
+    echo 'JSON NOT found' . PHP_EOL;
+} else {
+    echo 'JSON FOUND!' . PHP_EOL;
+}
+
+/* ------------------------------------------------------------------ */
+echo 'Hello World' . PHP_EOL;
+say_hello();
